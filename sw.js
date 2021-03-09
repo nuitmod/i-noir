a6addd650bab339e789742be35b8c02ad43b8b7c
nuitmod <wtvfqm@gmail.com>
2021-03-09 12:42:00 +0500

    ch index

---
+++ sw.js
@@ -0,0 +1,9 @@
+
+importScripts('https://cdn.ampproject.org/sw/amp-sw.js');
+//AMP_SW.init();
+AMP_SW.init({
+  assetCachingOptions: [{
+  regexp: /\.(png|jpg)/,
+  cachingStrategy: 'CACHE_FIRST'
+  }]
+});
