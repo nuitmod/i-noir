45dae46cee3f2d2197b9fbd0eddfe2ee8879d0d4
nuitmod <wtvfqm@gmail.com>
2021-04-13 17:14:22 +0500

    ch index

---
+++ api/dat.py
@@ -0,0 +1,8 @@
+from http.server import BaseHTTPRequestHandler
+from i_p import handler
+
+def ii():
+    print("innite9")
+
+
+#uu="info"
--- api/i_p.py
+++ api/i_p.py
@@ -1,11 +1,16 @@
 from http.server import BaseHTTPRequestHandler
 
+#from dat import ii, uu
+
 class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    msg="i-noir data"
+    msg="w-i-mod data"
     self.wfile.write(msg.encode())
     return
+
+
+#ii()
